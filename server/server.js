195022b532485a539d854e7988344851b8ef269f
raji1209 <rajalakshmiraja2005@gmail.com>
2026-06-15 17:31:29 +0530

    fix cors for vercel frontend

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ console.log("OPENAI KEY LOADED:", process.env.OPENAI_API_KEY ? "YES" : "NO")
 
 const app = express()
 
-app.use(cors({ origin: "http://localhost:5174", credentials: true }))
+app.use(cors({ origin: "http://localhost:5174","https://ai-career-hub-ten.vercel.app", credentials: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/uploads", express.static("uploads"))
